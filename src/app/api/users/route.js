e1de9e399b1e03609eaec513d546a5f5f9ced18d
Alokkumar-8467 <alokkumarcse01@gmail.com>
2024-03-06 15:53:28 +0530

    request-response done

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -25,6 +25,25 @@ export function DELETE(request){
     {status: 201, statusText: "hey Changed text"}
     );
 }
-export function POST(){}
+export async  function POST(request){
+    const body = request.body;
+    console.log(body);
+    console.log(request.method);
+    console.log(request.nextUrl.pathname);    
+    console.log(request.nextUrl.searchparam);
+
+    const jsonData = await request.json();
+    console.log(jsonData);
+
+    // const textData = await request.text();
+    // console.log(textData)
+
+
+
+    return NextResponse.json({
+        message : "posting user data",
+    });
+
+}
 export function PUT(){}
 export function UPDATE(){}
